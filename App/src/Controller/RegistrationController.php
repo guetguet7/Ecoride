a44ba5210241fef117385f34d00f5d461ba0802d
gaetan <gaetan.julien@outlook.com>
2025-12-09 16:05:43 +0100

    maj3

--- App/src/Controller/RegistrationController.php
+++ App/src/Controller/RegistrationController.php
@@ -47,7 +47,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('joe@gmail.com', 'Support Garden Time'))
+                    ->from(new Address('support@ecoride.com', 'Support Ecoride'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
